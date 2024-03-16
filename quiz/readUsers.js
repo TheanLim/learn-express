9cfb2aa491b6dd533bb9afd8a8414bc315a639dd
Thean Lim <limxx431@umn.edu>
2024-03-15 21:29:46 -0400

    Organized code using express router

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,25 @@
 const express = require('express')
 const router = express.Router();
+
+router.get('/usernames', (req, res) => {
+  let usernames = req.users.map(function(user) {
+    return {id: user.id, username: user.username};
+  });
+  res.send(usernames);
+});
+
+router.get('/username/:name', (req, res) => {
+    let user = req.users.filter(user => user.username === req.params.name);
+    if (user.length === 0){
+      res.send({
+        error: {
+          message: 'User not found',
+          status: 404
+        }
+      });
+    } else {
+      res.send(user);
+    }
+  });
+
+module.exports = router
